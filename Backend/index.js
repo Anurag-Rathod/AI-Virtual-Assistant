fe8e6956bf224f22be010c44662ac9da466a9e32
Anurag Rathod <anuragrathod999@gmail.com>
2026-09-19 14:54:44 +0530

    new commit

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+import geminiResponse from "./gemini.js";
+
+const app = express();
+dotenv.config();
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRouter);
+app.use("/api/user",userRouter);
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>{
+    console.log("Server started!");
+    connectDB();
+})
